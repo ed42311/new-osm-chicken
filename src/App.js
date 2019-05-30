ed119c0c4b56e5ee3d7d6fa1505cf74696b8e119
ed42311 <ed42311@gmail.com>
2019-05-29 19:45:19 -0600

    hello there

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import mapboxgl from 'mapbox-gl';
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+mapboxgl.accessToken = 'undefined'
+
+class MapBox extends Component {
+  componentDidMount() {
+    
+  }
+  render() {
+    const Map = ReactMapboxGl({
+      accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    });
+    return (
+
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+        height: "100vh",
+        width: "100vw"
+        }}>
+        <Layer
+          type="symbol"
+          id="marker"
+          layout={{ "icon-image": "marker-15" }}>
+          <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+        </Layer>
+    </Map>
+    )
+  }
+}
+
+class App extends Component {
+  componentDidMount() {
+    let query = `{
+      PointOfInterest(first:10, offset: 4) {
+        name
+        poi_id
+        shortestPathRouteToPOI(poi_id:"Mad Dog in the Fog37.7721448-122.4310153") {
+          lat
+          lon
+        }
+      }
+    }`
+    let body = {
+      operationName: null,
+      query: query,
+      variables: {}
+    }
+    fetch("https://osm-routing-api.now.sh/", 
+      {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+          'Content-Type' : 'application/json',
+          'Accept' : 'application/json',
+        }
+      })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.error(err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <MapBox />
+      </div>
+    );
+  }
+
 }
 
 export default App;
